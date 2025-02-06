1f8746b4905ac9d11725edd3a2837d665b69757f
SteveChoi1219 <stevechoi268@gmail.com>
2025-02-06 18:02:25 -0500

    Binary / linear search

--- DataAnalyzer.java
+++ DataAnalyzer.java
@@ -1,7 +1,20 @@
 public class DataAnalyzer {
     //binary
     public static int searchList(int target, int[] numbers) {
-        
+        int minIndex = 0;
+        int maxIndex = numbers.length - 1;
+        while(minIndex <= maxIndex) {
+            int middle = minIndex + (maxIndex - minIndex) / 2;
+            if(target == numbers[middle]) {
+                return middle;
+            } else {
+                if(target > numbers[middle]) {
+                    minIndex = middle + 1;
+                } else {
+                    maxIndex = middle - 1;
+                }
+            }
+        }
         return -1;
     }
 
@@ -21,6 +34,8 @@ public class DataAnalyzer {
 
     public static void main(String[] args) {
         int[] r = {10, 20, 30, 40, 50};
+        System.out.println(searchList(50, r));
+
         System.out.println(searchList(r, 50));
     }
 }
